3b7b6937fd2f6df19fee73a3e62aa3b8b4ddfea7
AndreyEpp <epp@mayak.help>
2022-11-18 16:35:50 +0300

    add new fixtures

--- tests/test_all.py
+++ tests/test_all.py
@@ -17,3 +17,13 @@ def test_diff_yml():
     actual = generate_diff('./tests/fixtures/file1.yml',
 			   './tests/fixtures/file2.yml')
     assert actual == expected.DIFF_YML
+
+def test_diff_compl():
+    actual = generate_diff('./tests/fixtures/file1_compl.json',
+                           './tests/fixtures/file2_compl.json')
+    assert actual == expected.DIFF_COMPL
+
+def test2_diff_compl():
+    actual = generate_diff('./tests/fixtures/file1_compl.yaml',
+                           './tests/fixtures/file2_compl.yaml')
+    assert actual == expected.DIFF_COMPL
